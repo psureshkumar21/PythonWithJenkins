379be1d41fc752a69364e7ef177358b4d647a8b1
SureshP <psureshkumar21@gmail.com>
2022-03-31 22:37:39 +0530

    Final Commit

--- Python1.py
+++ Python1.py
@@ -1,8 +1,1 @@
-#Arithmetic Operations On Python
-i=int(input("Enter First Number:"))
-j=int(input("Enter Second Number:"))
-print("Sum of two numbers is:",(i+j))
-print("Subtraction of two numbers is:",(i-j))
-print("Multiplication of two numbers is:",(i*j))
-print("Divison of two numbers is:",(i/j))
-print("Mod of two numbers is:",(i % j))
+print("Hello World.")

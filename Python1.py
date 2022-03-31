1053574a32257d49bbd70625fb36239e33635790
SureshP <psureshkumar21@gmail.com>
2022-03-31 21:53:47 +0530

    Adding Pythong Program

---
+++ Python1.py
@@ -0,0 +1,10 @@
+#Arithmetic Operations On Python
+#Hello welcome
+#-sk\
+i=int(input("Enter First Number:"))
+j=int(input("Enter Second Number:"))
+print("Sum of two numbers is:",(i+j))
+print("Subtraction of two numbers is:",(i-j))
+print("Multiplication of two numbers is:",(i*j))
+print("Divison of two numbers is:",(i/j))
+print("Mod of two numbers is:",(i % j))

f769a83f07f702cecc4ae37fd83532c6f115de5f
SureshP <psureshkumar21@gmail.com>
2022-04-01 10:59:49 +0530

    5th File to Git

---
+++ Python5.py
@@ -0,0 +1,8 @@
+#Arithmetic Operations
+a=50
+b=7
+print("Addition of a and b is:",a+b)
+print("Subtraction of a and b is:",a-b)
+print("Multiplication of a and b is:",a*b)
+print("Division of a and b is:",a/b)
+print("Modulus of a and b is:",a%b)
